18bd1a5894e721e39a47c69e2df28811abd26797
Ankitkumar147 <aankitchouhan3@gmail.com>
2023-02-07 02:18:26 +0530

    First push till home-Page Design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookComponent } from './component/book/book.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
+import { LoginComponent } from './component/login/login.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '',redirectTo: '/home-Page', pathMatch:'full'},
+  {path: 'home-page' , component: HomePageComponent},
+  {path: 'login' , component: LoginComponent},
+  {path: 'book', component: BookComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { LoginComponent } from './component/login/login.component';
+import { AlertComponent } from './component/alert/alert.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import { BookComponent } from './component/book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomePageComponent,
+    LoginComponent,
+    AlertComponent,
+    BookComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
